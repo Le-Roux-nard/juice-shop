8537922a894bea2951e22280f99a029c5db20b73
Le-Roux-Nard <lerouxnard@lerouxnard.fr>
2025-11-04 16:42:53 +0100

    removed unused CI

--- .githooks/pre-commit.d/01-linter.sh
+++ .githooks/pre-commit.d/01-linter.sh
@@ -7,9 +7,25 @@ NC='\033[0m' # No Color
 # Linter
 ###########################################
 
-        if command -v npx >/dev/null 2>&1; then
-    echo "→ Formatage du code avec Prettier..."
-    npx prettier --write .
-else
-    echo -e "${RED}⚠️  npx non trouvé. Installe Node.js/PNPM/Yarn.${NC}"
+if [ -f "package.json" ]; then
+    if command -v npx >/dev/null 2>&1; then
+        echo "→ Formatage du code avec Prettier..."
+        npx prettier --write .
+        echo "→ Lint du code avec ESLint..."
+        npx eslint . --fix
+    else
+                echo -e "${RED}⚠️  npx non trouvé. Installe Node.js/PNPM/Yarn.${NC}"
+    fi
+fi
+
+# Exemple Python
+if [ -f "pyproject.toml" ] || [ -f "requirements.txt" ]; then
+    if command -v black >/dev/null 2>&1; then
+        echo "→ Formatage Python avec Black..."
+        black .
+    fi
+    if command -v ruff >/dev/null 2>&1; then
+        echo "→ Lint Python avec Ruff..."
+        ruff check . --fix
+    fi
 fi
